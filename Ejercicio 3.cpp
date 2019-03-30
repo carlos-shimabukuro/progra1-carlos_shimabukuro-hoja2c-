9c365ef811a0be0ff79b4dc09c48a455b3663083
carlos-shimabukuro <48858772+carlos-shimabukuro@users.noreply.github.com>
2019-03-30 08:56:11 -0500

    Add files via upload

---
+++ Ejercicio 3.cpp
@@ -0,0 +1,49 @@
+// ConsoleApplication3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+//Test
+/*
+test1
+*/
+
+int main()
+{
+//Entrada
+	int hora;
+	int minutos;
+	int segundos;
+
+//Restricciones
+ /*
+ hora < 24
+ minutos && segundos < 60
+ */
+//Logica
+	cout << "Hora:" << endl;
+	cin >> "hora:";
+	cout << "Minutos:" << endl;
+	cin >> "minutos:";
+	cout << "Segundos:" << endl;
+	cin >> "segundos:";
+
+	if (segundos = 60)
+	{
+		minutos++
+	}
+	if minutos = 60
+	{
+		hora++
+	}
+
+//Salida 
+	cout << hora << ":" << minutos << ":" << segundos << ":"
+
+		_getch();
+}
+
